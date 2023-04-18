9c3926d209f6460b6591ba61afb2b2cef123a8ab
islam soliman <islam.soliman1990@gmail.com>
2023-04-18 07:13:00 +0200

    v1

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/islamsoliman90/portfolio/main/src/Assets/Islam_Ahmed-Giza_Egypt.pdf";
+  "https://raw.githubusercontent.com/islamsoliman90/Cv/main/Islam_Ahmed-Giza_Egypt.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
